f97da236a8822a482c271bc5d49d14658c1adbe7
Jamie Nagy <yuqiye093@gmail.com>
2024-05-16 20:19:10 -0400

    tweak

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import ImageSlideshow from '@/components/images/image-slideshow';
-import classes from './page.module.css';
+import ImageSlideshow from "@/components/images/image-slideshow";
+import classes from "./page.module.css";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         </div>
         <div>
           <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <h1>New level Foodies for New level Meals!</h1>
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
@@ -25,26 +25,26 @@ export default function Home() {
         <section className={classes.section}>
           <h2>How it works</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
+            Foodies is a platform for foodies to share their favorite recipes
+            with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
+            Foodies is a place to discover new dishes, and to connect with other
+            food lovers.
           </p>
         </section>
 
         <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
+          <h2>Why Foodies?</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
+            Foodies is a platform for foodies to share their favorite recipes
+            with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
+            Foodies is a place to discover new dishes, and to connect with other
+            food lovers.
           </p>
         </section>
       </main>
